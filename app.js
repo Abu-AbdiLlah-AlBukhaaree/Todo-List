f76067b0e7c10bbd71af61e1b47ae651002f6e37
unknown <faisolbamigbade@gmail.com>
2023-06-13 13:32:08 +0100

    finisshed setting-up style.css

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const addItem = document.getElementById('add-item');
-const btn = document.querySelector('.add-activity');
+const btn = document.querySelector('.add-activity-btn');
 const btnRect = btn.getBoundingClientRect();
 
 addItem.style.width = `calc(100% - ${btnRect.width}px)`;
